488db86cffa011601e5b2679cc6076d9b5a2728c
Dinesh <dinesh.p@caratlane.com>
2021-01-09 21:32:17 +0530

    Initial Commit

---
+++ SaltSideDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaltSideDemo
+//
+//  Created by Dinesh on 09/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
